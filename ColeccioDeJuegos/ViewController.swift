74f98fff80e99f15d36579f9a8c3c6156f7c1851
Xiomara <xiomara.torres@tecsup.edu.pe>
2020-05-27 11:51:01 -0500

    Initial Commit

---
+++ ColeccioDeJuegos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ColeccioDeJuegos
+//
+//  Created by mbtec22 on 5/27/20.
+//  Copyright © 2020 xiomara. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
